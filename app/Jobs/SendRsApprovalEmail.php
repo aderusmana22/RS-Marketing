d09d4a92173304017a00e17f6d5f47e8a9f76ec3
Zack <zakiwirawan25@gmail.com>
2025-06-02 11:57:18 +0700

    Enhance RsApprovalMail and SendRsApprovalEmail; add logging for better traceability and update email view

--- app/Jobs/SendRsApprovalEmail.php
+++ app/Jobs/SendRsApprovalEmail.php
@@ -5,7 +5,7 @@ namespace App\Jobs;
 use App\Mail\RsApprovalMail;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -28,10 +28,15 @@ class SendRsApprovalEmail implements ShouldQueue
         $this->rsMaster = $rsMaster;
         $this->approvalToken = $approvalToken;
         $this->rejectToken = $rejectToken;
+
+        log::info('SendRsApprovalEmail job created for approver: ' . $approver->email);
+        log::info('Requisition Slip Master ID: ' . json_encode($rsMaster));
+        log::info('Approval Token: ' . $approvalToken);
+        log::info('Reject Token: ' . $rejectToken);
     }
 
     public function handle()
     {
-        Mail::to($this->approver->email)->send(new RsApprovalMail($this->rsMaster, $this->approvalToken, $this->rejectToken));
+        Mail::to($this->approver->email)->send(new RsApprovalMail($this->approver, $this->rsMaster, $this->approvalToken, $this->rejectToken));
     }
 }
--- app/Mail/RsApprovalMail.php
+++ app/Mail/RsApprovalMail.php
@@ -12,21 +12,28 @@ class RsApprovalMail extends Mailable
     use Queueable, SerializesModels;
 
     public $rsMaster;
+    public $approver;
     public $approvalToken;
     public $rejectToken;
 
-    public function __construct($rsMaster, $approvalToken, $rejectToken)
+    public function __construct($approver, $rsMaster, $approvalToken, $rejectToken)
     {
         $this->rsMaster = $rsMaster;
+        $this->approver = $approver;
         $this->approvalToken = $approvalToken;
         $this->rejectToken = $rejectToken;
+        log::info('RsApprovalMail created for approver mail: ' . $approver->name);
+        log::info('Requisition Slip Master ID mail: ' . json_encode($rsMaster));
+        log::info('Approval Token mail: ' . $approvalToken);
+        log::info('Reject Token mail: ' . $rejectToken);
     }
 
     public function build()
     {return $this->subject('Approval Request for Requisition Slip')
-        ->view('emails.rs_approval')
+        ->view('mail.sendNotifAprroval')
         ->with([
-            'rsMaster' => $this->rsMaster,
+            'rsMaster' => $this->rsMaster, 
+            'approver' => $this->approver,
             'approvalUrl' => route('rs.approve', ['token' => $this->approvalToken]),
             'rejectUrl' => route('rs.reject', ['token' => $this->rejectToken]),
         ]);
