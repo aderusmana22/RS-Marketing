bff988ead27b1d269382203c4a80aaa7064f0510
Zack <zakiwirawan25@gmail.com>
2025-06-23 07:42:18 +0700

    Update dashboard card titles; change 'Monthly Requisitions' to 'Report Requisitions' and 'Pending' to 'Monthly Overview'

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -69,14 +69,14 @@
                 <div class="card-body">
                     <div class="row align-items-start">
                         <div class="col-8">
-                            <h4 class="card-title mb-9 fw-semibold">Monthly Requisitions</h4>
+                            <h4 class="card-title mb-9 fw-semibold">Report Requisitions</h4>
                             <div class="d-flex align-items-center mb-3">
-                                <h4 class="fw-semibold mb-0 me-8">Pending</h4>
+                                <h4 class="fw-semibold mb-0 me-8">Monthly Overview</h4>
                                 <div class="d-flex align-items-center">
                                     <span class="me-2 rounded-circle bg-success-subtle text-success round-20 d-flex align-items-center justify-content-center">
                                         <i class="ti ti-arrow-up-left"></i>
                                     </span>
-                                    <p class="text-dark me-1 fs-3 mb-0">+9%</p>
+                                    <p class="text-dark me-1 fs-3 mb-0"></p>
                                 </div>
                             </div>
                         </div>
